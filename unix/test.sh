1580b960408facef742d49cc1438ec7c1e8a6ce5
kenjimorita <kenjimoritata@gmail.com>
2021-01-10 18:51:17 +0900

    add

--- unix/test.sh
+++ unix/test.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-echo "Hello, $HOME World!"
+HOGE="hoge" # 変数HOGEにhogeを代入している。=の後は空白なし。くっつける
+FUGA="fuga" # 変数FUGAにfugaを代入している
+
+echo $HOGE$FUGA # 変数同士連結する場合はくっつける
+
+NAME="kenji"
+echo "Hello! $NAME" # ダブルクォーテーションでは式が展開されるが...
+echo 'Hello! $NAME'
