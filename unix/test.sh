677e3d306a914b3546791c37abcf79d8e452f2d4
kenjimorita <kenjimoritata@gmail.com>
2021-01-11 23:23:54 +0900

    add

--- unix/test.sh
+++ unix/test.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-HelloFunction () {
-  echo "hello $2 $1!!"
-}
-
-HelloFunction kenji morita
+index=1
+for file in *.png
+do
+  mv "$file" "./img${index}.jpg"
+  index=`expr $index + 1`
+done
