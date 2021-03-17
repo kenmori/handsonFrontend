42785c4a115f12182cf4d27ec73f62dde0a7ce64
kenmori <kenjimoritata@gmail.com>
2021-03-17 23:20:40 +0900

    fix

--- work/markup/answer/table/src/index.ts
+++ work/markup/answer/table/src/index.ts
@@ -3,41 +3,51 @@ import "babel-polyfill"
 type User = { id: number; name: string; gender: string; age: number };
 type Users = User[];
 
+
+
 class Table {
   target: HTMLElement
   table: HTMLElement
+  tbody: HTMLElement | null
   users: Users
   currentUsers: Users
   headers: string[]
   constructor(target: HTMLElement, users: Users, headers: string[]) {
     this.target = target;
-    this.table = document.createElement("table");
+    this.table = document.createElement("table")
+    this.table.setAttribute("id", "table")
+    this.tbody = document.createElement("tbody");
+    this.tbody.setAttribute("id", "tbody")
     this.users = users;
     this.currentUsers = users;
     this.headers = headers;
     this.createHeaders();
-    this.showTable(this.users);
   }
-  createSortView() {
-    const idEle = document.getElementById("js-sort-id");
-    if(idEle) idEle.textContent = "▼";
-  }
+
   createHeaders() {
     const thead = document.createElement("thead");
     const headerTr = document.createElement("tr");
     const frag = document.createDocumentFragment();
     for (let header of this.headers) {
       const th = document.createElement("th");
-      th.id = `js-sort-${header}`;
+      th.id = `js-${header}`
       th.textContent = header;
       frag.append(th);
     }
     this.table.appendChild(thead).appendChild(headerTr).appendChild(frag);
   }
+  updateTable(users: Users){
+    const tbody = document.getElementById("tbody")
+    const childNodes = tbody?.childNodes ?? []
+    childNodes.forEach((node: ChildNode, i: number) => {
+      node.childNodes[0].textContent = String(users[i].id)
+      node.childNodes[1].textContent = String(users[i].name)
+      node.childNodes[2].textContent = String(users[i].gender)
+      node.childNodes[3].textContent = String(users[i].age)
+    })
+  }
   showTable(users: Users) {
-    const tbody = document.createElement("tbody");
     for (let user of users) {
-      console.log(user);
       const frag = document.createDocumentFragment();
       const userTr = document.createElement("tr");
       const tdId = document.createElement("td");
@@ -53,16 +63,83 @@ class Table {
       frag.appendChild(tdGender);
       frag.appendChild(tdAge);
       userTr.appendChild(frag);
-      tbody.appendChild(userTr);
-      this.table.appendChild(tbody);
+      this.tbody?.appendChild(userTr);
+      this.table.appendChild(this.tbody ?? document.createElement("span"));
     }
     this.target.appendChild(this.table);
   }
-  set data(users: Users) {
+  setUsers(users: Users) {
     this.users = users;
   }
-  get data() {
+  getUsers() {
     return this.users;
+  }
+}
+
+
+class SortTable extends Table {
+  headers: string[]
+  currentSort: "ASC" | "DESC" | "NORMAL"
+  users: Users
+  static sortType = {ASC: "ASC", DESC: "DESC", NORMAL: "NORMAL"} as const
+  constructor(target: HTMLElement, users: Users, headers: string[]){
+    super(target, users, headers);
+    this.currentSort = SortTable.sortType.NORMAL
+  }
+  callback = (event: MouseEvent) => {
+    console.log(event.target);
+      if(event.target === null)  return
+      const targetSort = (event.target as HTMLElement).getAttribute("data-sort") as "id" | "age"
+      const sortStatus = (event.target as HTMLElement).getAttribute("data-status") as "ASC" | "DESC" | "NORMAL" | null
+      console.log(targetSort, sortStatus);
+      if(targetSort === null) return
+      let sortedData = this.getUsers();
+      switch(sortStatus){
+        case "NORMAL":
+          (event.target as HTMLElement).setAttribute("data-status", "ASC");
+          (event.target as HTMLElement).textContent = `${targetSort}▼`
+        case "ASC":
+          sortedData = [...this.getUsers()].sort((a, b) => {
+          return a[targetSort] - b[targetSort]
+        });
+        (event.target as HTMLElement).setAttribute("data-status", "DESC");
+        (event.target as HTMLElement).textContent = `${targetSort}▲`
+        case "DESC":
+          sortedData = [...this.getUsers()].sort((a, b) => {
+            return b[targetSort] - a[targetSort]
+          });
+          (event.target as HTMLElement).setAttribute("data-status", "NORMAL");
+          (event.target as HTMLElement).textContent = `${targetSort}+`
+      }
+      super.updateTable(sortedData)
+    }
+  attachEvent(){
+    this.headers.forEach(header => {
+      const th = document.getElementById(`js-${header}`)
+      th?.addEventListener("click", this.callback)
+    })
+  }
+  createSortView(headers?: string[]) {
+    if(!headers) return
+    headers.forEach((header) => {
+      const sortTarget = document.getElementById(`js-${header}`);
+      if(sortTarget) sortTarget.textContent += "+";
+      if(sortTarget) sortTarget.setAttribute("data-status", "NORMAL");
+      if(sortTarget) sortTarget.setAttribute("data-sort", `${header}`)
+    })
+  }
+}
+
+class PagenationTable extends SortTable {
+  static Limit = 5
+  target: HTMLElement
+  headers: string[]
+  users: Users
+  constructor(target: HTMLElement, users: Users, headers: string[]){
+    super(target, users, headers)
+  }
+  createPagenationView(){
+
   }
 }
 
@@ -75,11 +152,21 @@ async function init() {
   const res = await request(
     "https://jsondata.okiba.me/v1/json/OSJpx210315100914"
   );
-  new Table(document.getElementById("app")!!, res.data, [
+  return res
+
+}
+async function controller() {
+  const res = await init();
+  const sortTableIns = new PagenationTable(document.getElementById("app")!!, res.data, [
     "id",
     "name",
     "gender",
     "age"
-  ]);
+  ])
+  sortTableIns.showTable(res.data)
+  sortTableIns.createSortView(["id", "age"])
+  sortTableIns.attachEvent()
+  // const pagenation = new Pagenation(tableInstance)
 }
-init();
+
+controller()
